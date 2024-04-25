b1d6d9b3709be7633d7fb0d5e097e2fc9e29a442
ntsatsura <it.tester.qa@gmail.com>
2024-04-26 01:42:03 +0200

    Added hw-2.1.4

--- task_score.js
+++ task_score.js
@@ -1,14 +1,16 @@
 //расчет среднего балла за домашние задания студента по модулям
 let nameStudent = "Nastya";
+let newNameStudent = nameStudent.slice(0,1);
 let surnameStudent = "Tsatsura";
-let fullNameStudent = nameStudent + " " + surnameStudent
 
-let moduleIntro = 20
-let moduleGit = 30
-let moduleJs = 30
+let index = 0;
 
-let total = moduleIntro + moduleGit + moduleJs
+let moduleIntro = 20;
+let moduleGit = 30;
+let moduleJs = 30;
+
+let total = moduleIntro + moduleGit + moduleJs;
 
 let averageMark = Math.ceil(total /3);
 
-console.log(`The ${fullNameStudent} average mark is ${averageMark}`)
+console.log(`The ${newNameStudent}. ${surnameStudent} average mark is ${averageMark}`);
