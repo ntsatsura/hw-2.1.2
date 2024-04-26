9404147724bb0c46c36daf0ad3401ec29e9c35d1
ntsatsura <it.tester.qa@gmail.com>
2024-04-26 07:45:17 +0200

    Modified files

--- task_score.js
+++ task_score.js
@@ -3,13 +3,11 @@ let nameStudent = "Nastya";
 let newNameStudent = nameStudent.slice(0,1);
 let surnameStudent = "Tsatsura";
 
-let index = 0;
+const introScore = 180;
+const gitScore = 180;
+const jsScore = 180;
 
-let moduleIntro = 20;
-let moduleGit = 30;
-let moduleJs = 30;
-
-let total = moduleIntro + moduleGit + moduleJs;
+let total = introScore + gitScore + jsScore
 
 let averageMark = Math.ceil(total /3);
 
