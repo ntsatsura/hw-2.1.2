77e1ac3793644e1bc5fffbcf97cbb3ab0e6192a2
ntsatsura <it.tester.qa@gmail.com>
2024-04-28 07:33:30 +0200

    Corrected hw-2.1.4

--- task_score.js
+++ task_score.js
@@ -3,10 +3,10 @@ let nameStudent = "Nastya";
 let newNameStudent = nameStudent.slice(0,1);
 let surnameStudent = "Tsatsura";
 
-const introScore = 105;
-const gitScore = 177;
-const jsScore = 180;
+let moduleIntro = 20;
+let moduleGit = 30;
+let moduleJs = 30;
 
-let averageMark = (introScore + gitScore + jsScore) / 3;
+let averageMark = Math.ceil((moduleIntro + moduleGit + moduleJs) / 3);
 
 console.log(`The ${newNameStudent}. ${surnameStudent} average mark is ${averageMark}`);
